bab27e567b3c2327b87322a42f6f313b3c63312b
Facundo Vandecaveye <facundovs84@hotmail.com.ar>
2023-06-27 12:18:44 -0300

    Creada las rutas de la aplicacion

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,21 @@
+import { Route } from "react-router-dom"
 import { Routes } from "react-router-dom"
+import { LoginPage } from "../auth"
+import { CalendarPage } from "../calendar"
+import { Navigate } from "react-router-dom"
 
 export const AppRouter = () => {
+    
+    const authStatus = 'not-authenticated'
+
     return (
         <Routes>
-            
+            {
+                (authStatus === 'not-authenticated') 
+                ? <Route path="/auth/*" element={<LoginPage/>}/>
+                : <Route path="/*" element={<CalendarPage/>}/>
+            }
+            <Route path="/*" element={<Navigate to="/auth/login"/>}/>
         </Routes>
     )
 }
