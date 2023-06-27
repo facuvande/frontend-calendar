6e03d3e63b2f6362ee697943ee08cfefdc77995a
Facundo Vandecaveye <facundovs84@hotmail.com.ar>
2023-06-27 13:00:23 -0300

    Optimizacion del calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,22 +1,10 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
-
+import { addHours } from 'date-fns'
 import { Navbar } from "../"
 
-const locales = {
-    'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-})
+import { localizer, getMessagesEs } from '../../helpers'
 
 const events = [{
     title: 'Cumpleanos del Jefe',
@@ -31,16 +19,35 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+    
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        console.log({event, start, end, isSelected})
+
+        const style = {
+            backgroundColor: '#247CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white',            
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <>
             <Navbar/>
             <div>
                 <Calendar
+                    culture='es'
                     localizer={localizer}
                     events={events}
                     startAccessor="start"
                     endAccessor="end"
                     style={{ height: 'calc(100vh - 80px)' }}
+                    messages={getMessagesEs()}
+                    eventPropGetter={ eventStyleGetter }
                 />
             </div>
         </>
